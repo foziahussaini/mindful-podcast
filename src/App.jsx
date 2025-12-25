b8fff98a31a3e1eadb360786fd96f94d4c757c98
foziahussaini <fauziakhudadad@gmail.com>
2025-12-26 00:12:30 +0430

    complete header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Header from './component/header' 
+import { useState } from 'react';
+import Header from './component/header';
 import './App.css'
 
 function App() {
@@ -9,7 +7,7 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header/>
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,20 +1,39 @@
-import logo from '../assets/logo.png'
+import logoImage from '../assets/logo.png';
+import discordlogo from '../assets/discord.svg';
+import instagramlogo from '../assets/instagram.jpg';
+import githublogo from '../assets/gitlogo.png';
 function Header(){
     return (
         <header className="header">
             <div className='logo-content'>
-                <img className='image' src={"./src/assets/logo.png"} alt="" />
-                <h3 className='title'>Mindful Minutes</h3>
+                <img className='image' src={logoImage} alt="" />
+                <div className='title'> 
+                    <h3>Mindful</h3> 
+                    <h6>Minutes</h6>
+                </div>
             </div>
-            <ul>
-                <li><a>Home</a></li>
-                <li><a>About</a></li>
-                <li><a>Contact</a></li>
-                <li><a>dashboard</a></li>
+            <ul className='menu'>
+                <li className='menu-bar'><a>Home</a></li>
+                <li className="menu-bar"><a>About</a></li>
+                <li className="menu-bar"><a>Contact</a></li>
+                <li className="menu-bar"><a>Episodes</a></li>
             </ul>
-            <ul>
-                <li>https://discord.gg/pAG6Eytj</li>
-                <li><i class="fa-brands fa-discord"></i></li>
+            <ul className="social-icons">
+                <li className="icon">
+                    <a href="https://discord.gg/pAG6Eytj" target="_blank" >
+                        <img src={discordlogo} className="logo" alt="discord" />
+                    </a>
+                </li>
+                <li className="icon">
+                    <a href="https://github.com/foziahussaini" target="_blank" >
+                        <img src={githublogo} className="github" alt="github" />
+                    </a>
+                </li>    
+                <li className="icon">
+                    <a href="#" target="_blank" >
+                        <img src={instagramlogo} className="instagram" alt="instagram" />
+                    </a>
+                </li>    
             </ul>
         </header>
     )
